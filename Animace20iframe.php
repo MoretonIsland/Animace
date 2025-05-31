e18038826733bd69e13ea6315bcdef8b46a4f4e9
MoretonIsland <107469969+MoretonIsland@users.noreply.github.com>
2025-06-01 01:35:28 +0200

    Add files via upload

--- Animace20iframe.php
+++ Animace20iframe.php
@@ -1,28 +1,45 @@
 <style>
-    html, body {
-        margin: 0;
-        padding: 0;
-        height: 100%;
-        background-color: black;
-        overflow: hidden;
-    }
+      body {
+          height: 100vh;
+          overflow: hidden;
+          padding: 2rem;
+          background: radial-gradient(129% 99% at 112% 85%, rgb(223, 220, 255) 20%, rgb(166, 158, 255) 90%),    
+            url('https://assets.codepen.io/16327/noise-e82662fe.png');  
+          background-blend-mode: color-dodge;
+        }
 
-    .container {
-        width: 100%;
-        height: 100%;
-        position: relative;
-    }
+        .split {
+          -webkit-transform: translate3d(0, 0, 0);
+          font-size: 24px;
+          line-height: 36px;
+          color: #dedede;
+          margin: 50px auto;
+          width: 800px;
+          visibility: hidden;
+        }
 
-    canvas {
-        width: 100%;
-        height: 100%;
-        display: block;
-        background-color: black;
-    }
+        .split div {
+          border-radius: 4px;
+          background: var(--color-surface-white);
+          padding: 0 8px;
+          margin: 4px 0;
+          -webkit-font-smoothing: antialiased;
+          -moz-font-smoothing: antialiased;
+          color: var(--color-just-black);
+          font-weight: semibold;
+          box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+        }
 </style>
+<body>
+ <div class="split" id="quote">Varnsdorf Rumburk Děčín Šluknov Rybniště Chřibská</div>
+    <div class="split" id="decoys">České Budějovice Hradec Králové Zlín Pardubice Ústí nad Labem Karlovy Vary</div>  
 
-<div class="container">
-    <canvas id="canvas"></canvas>
-</div>
+    <!-- Připojení JS souboru -->
+    <script src="script39.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.13.0/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.13.0/SplitText.min.js"></script>
 
-<script src="script20.js"></script>
+<script src="script39.js"></script>
+
+</body>
+</html>
