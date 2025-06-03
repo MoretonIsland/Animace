21a5d4d7f187daec31b630bbaf82f4a7a4d6bc90
MoretonIsland <107469969+MoretonIsland@users.noreply.github.com>
2025-06-03 11:26:05 +0200

    Add files via upload

---
+++ KnihovnaThree1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Three.js Noise Plane</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Odkaz na externí CSS -->
+  <link rel="stylesheet" href="styles43.css">
+
+  <!-- THREE.js -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
+
+  <!-- Chroma.js -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/chroma-js/2.1.0/chroma.min.js"></script>
+
+  <!-- Simplex Noise -->
+  <script src="https://cdn.jsdelivr.net/npm/simplex-noise@3.0.0/simplex-noise.min.js"></script>
+</head>
+<body>
+
+<canvas id="background"></canvas>
+<script type="module" src="script43.js"></script>
+
+</body>
+</html>
