27150fb4729916634dd9d99115b2064399d3abdc
MoretonIsland <107469969+MoretonIsland@users.noreply.github.com>
2025-06-04 01:41:06 +0200

    Add files via upload

---
+++ header-KnihovnaThree.php
@@ -0,0 +1,59 @@
+<header>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700;800;900&display=swap" rel="stylesheet">
+
+  <style>
+    body {
+      background-color: white;
+      font-family: 'Open Sans', sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+
+    header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 15px 30px;
+      background-color: #ffffff;
+    }
+
+    header h1 {
+  margin: 0 0 0 0px; /* nahoře 0, vpravo 0, dole 0, vlevo 40px odsazení */
+  color: #414f9e;
+  font-size: 40px;
+  font-family: 'Open Sans', sans-serif;
+  font-weight: bold;
+}
+
+
+    .header-right {
+      display: flex;
+      align-items: center;
+    }
+
+    .header-right a {
+      color: #555555;
+      text-decoration: none;
+      margin-left: 20px;
+      font-size: 18px;
+      transition: color 0.3s ease;
+      font-family: 'Open Sans', sans-serif;
+    }
+
+    .header-right a:hover {
+      color: #414f9e;
+    }
+  </style>
+
+  <div>
+    <h1>3D animace - knihovna Three.js</h1>
+  </div>
+
+  <div class="header-right">
+    <a href="index.php">Domů</a>
+    <a href="galerie.php">Animace</a>
+    <a href="kontakt.php">Kontakt</a>
+    <a href="onas.php">O nás</a>
+    <a href="blog.php">Blog</a>
+  </div>
+</header>
